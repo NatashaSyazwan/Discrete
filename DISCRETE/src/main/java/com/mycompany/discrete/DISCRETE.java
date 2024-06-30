c1156c274eb1823a91af2ae78a4789e8f3b321ad
NatashaSyazwan <111846354+NatashaSyazwan@users.noreply.github.com>
2024-06-30 11:12:14 +0800

    upload

---
+++ DISCRETE/src/main/java/com/mycompany/discrete/DISCRETE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.discrete;
+
+/**
+ *
+ * @author natas
+ */
+public class DISCRETE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
